65a1c5a65280842902a3930ece41ff4f044857cf
Aurélie VIOLETTE <orwel.violette@gmail.com>
2024-09-25 08:56:51 +0200

    Handle removing of wrapper (#12)

--- ww-config.js
+++ ww-config.js
@@ -8,6 +8,9 @@ export default {
         { name: 'change', label: { en: 'On change' }, event: { value: '' }, default: true },
         { name: 'initValueChange', label: { en: 'On init value change' }, event: { value: '' } },
     ],
+    options: {
+        displayAllowedValues: ['flex', 'inline-flex'],
+    },
     properties: {
         button: {
             defaultValue: { isWwObject: true, type: 'ww-button' },
